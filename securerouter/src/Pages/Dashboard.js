aa18f451241910581930ae7ea3f19b469fa8febd
santhosh <santhoshkaliannann@gmail.com>
2022-10-14 18:49:49 +0530

    Remove function in dashboard page

--- securerouter/src/Pages/Dashboard.js
+++ securerouter/src/Pages/Dashboard.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-function Dashboard(){
-    const pagemove=()=>{
-        Navigate('/Home')
-    }
+
     return(
         <div>
             <h1>
                 Welcome to Dashboard Page
             </h1>
-            <button onClick={pagemove}>Dashboard</button>
+            <button>Dashboard</button>
             
         </div>
     )
